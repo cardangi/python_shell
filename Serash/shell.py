46516aba9373b4857b21ee0d72d8c255f4c371ca
Carlos <car.dangi@hotmail.com>
2017-01-11 20:20:56 -0200

    Some changes
    Initial DEF for some help to "Know-how", to teach people use (base args tuto for main user), and because you imported SYS, i used SYS.ARGV for options to user ( example: shell.py -g or shell.py -l will do something ), and trash colleting

--- Serash/shell.py
+++ Serash/shell.py
@@ -3,40 +3,59 @@ import sys, shlex, os
 SHELL_STATUS_RUN = 1
 SHELL_STATUS_STOP = 0
 
+def main():
+    print ("Users command:")
+    print ("-G = edit here")
+    print ("-L = edit here")
+    print ("-O = edit here")
+
+try:
+    if sys.argv[1] == "-G":
+        ### EDIT HERE ###
+    if sys.argv[1] == "-L":
+        ### EDIT HERE ###
+    if sys.argv[1] == "-O":
+        ### EDIT HERE ###
+except IndexError:
+    main()
+    
 #Splitting the user's command
 #(mkdir "my folder") will return a list ['mkdir', '"my folder"]
-def tokenize(string):
-    return shlex.split(string)
+try:
+    def tokenize(string):
+        return shlex.split(string)
 
-def execute(cmd_tokens):
-    pid = os.fork()
+    def execute(cmd_tokens):
+        pid = os.fork()
 
-    if pid == 0:
-        os.execvp(cmd_tokens[0], cmd_tokens)
-    elif pid > 0:
-        while True:
-            wpid, status = os.waitpid(pid, 0)
+        if pid == 0:
+            os.execvp(cmd_tokens[0], cmd_tokens)
+        elif pid > 0:
+            while True:
+                wpid, status = os.waitpid(pid, 0)
 
-            if os.WIFEXITED(status) or os.WIFSIGNALED(status):
-                break
-            
-    return SHELL_STATUS_RUN
+                if os.WIFEXITED(status) or os.WIFSIGNALED(status):
+                    break
 
-def shell_loop():
-    status = SHELL_STATUS_RUN
+        return SHELL_STATUS_RUN
 
-    while status == SHELL_STATUS_RUN:
-        sys.stdout.write('> ')
-        sys.stdout.flush()
+    def shell_loop():
+        status = SHELL_STATUS_RUN
 
-        cmd = sys.stdin.readline()
+        while status == SHELL_STATUS_RUN:
+            sys.stdout.write('> ')
+            sys.stdout.flush()
 
-        cmd_tokens = tokenize(cmd)
+            cmd = sys.stdin.readline()
 
-        status = execute(cmd_tokens)
+            cmd_tokens = tokenize(cmd)
 
-def main():
-    shell_loop()
+            status = execute(cmd_tokens)
 
-if __name__ == "__main__":
-    main()
+    def main():
+        shell_loop()
+
+    if __name__ == "__main__":
+        main()
+except:
+    ### Trash collection / crash prevention. If it crash, you copy the except and put here^ ###
